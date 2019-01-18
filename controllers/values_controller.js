ad19236846fcd6c069d1e75a2c06f3588cb332b8
Vinorth Vigneswaramoorthy <vinorthv@gmail.com>
2019-01-18 15:56:16 -0500

    Delete endpoint logic

--- controllers/values_controller.js
+++ controllers/values_controller.js
@@ -9,7 +9,7 @@ module.exports = {
             if (err) return handleError(err);
           });
 
-        res.status(200).send();
+        res.status(201).send();
     },
     readValues(req, res) {
         var Value = mongoose.model('Value', ValueSchema);
@@ -21,12 +21,16 @@ module.exports = {
         var Value = mongoose.model('Value', ValueSchema);
         
         Value.findOneAndUpdate({ id: req.body.id }, { title: req.body.title })
-        .then(() => Value.findOne({
-            id: req.body.id
-          }))
-          .then(updatedDocument => res.send(updatedDocument));
+                .then(() => Value.findOne({
+                        id: req.body.id
+                    }))
+                    .then(updatedDocument => res.send(updatedDocument));
     },
     deleteValue(req, res) {
+        var Value = mongoose.model('Value', ValueSchema);
 
+        Value.remove({ id: req.params.id })
+            .then(() => res.status(204).send())
+            
     }
 }
