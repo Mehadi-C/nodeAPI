2f99605a35a5547c7287dcfe7c1c7945d7c0b1f9
Vinorth Vigneswaramoorthy <vinorthv@gmail.com>
2019-02-16 15:18:21 -0500

    Update readme

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # nodeAPI
 JavaScript REST API using the Node
+
+Run:
+
+npm install  - Install all dependencies
+
+npm start - Start express server
+
+get '/test/:id'
+
+get '/tests'
+
+post '/test'
+
+post '/tests'
+
+put '/test'
+
+delete '/test/:id'
--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,9 @@
 const ValuesCtrl = require('../controllers/values_controller');
+var path = require('path');
 
 module.exports = function (app) {
     app
+     .get('/', (req, res) => res.sendFile(path.join(__dirname + '../../index.html')) )
      .get('/test/:id', ValuesCtrl.readValue)
      .get('/tests', ValuesCtrl.readValues)
      .post('/test', ValuesCtrl.createValue)
